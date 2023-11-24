53c4a5929920c024fd45f45bdd413a24025036b2
iv_rudneva <iv_rudneva@bingo-boom.com>
2023-11-24 11:34:22 +0300

    move comments to feed

--- src/components/feed/Feed.vue
+++ src/components/feed/Feed.vue
@@ -1,30 +1,34 @@
 <template>
-    <article class="feed">
-        <div class="feed__user">
-            <User :username="username" :avatar="avatar" />
+  <article class="feed">
+    <div class="feed__user">
+      <User :username="username" :avatar="avatar" />
+    </div>
+    <div class="feed__hero">
+      <slot name="s-story"></slot>
+    </div>
+    <div class="feed__comments" v-if="comments">
+      <Toggler @onToggle="toggle"></Toggler>
+      <div v-if="shown">
+        <div v-for="comment in comments" :key="comment">
+          <Comment :username="comment.username" :text="comment.text" />
         </div>
-        <div class="feed__hero">
-            <slot name="s-story"></slot>
-        </div>
-        <div class="feed__comments">
-            <Toggler @onToggle="toggle"></Toggler>
-            <div v-if="shown">
-                <slot name="s-comments"></slot>
-            </div>
-        </div>
-        <time class="feed__date">{{ date }} </time>
-    </article>
+      </div>
+    </div>
+    <time class="feed__date">{{ date }} </time>
+  </article>
 </template>
 
 <script>
 import { Toggler } from '../toggler'
 import { User } from '../user'
+import { Comment } from '../comment'
 
 export default {
   name: 'feed',
   components: {
     Toggler,
-    User
+    User,
+    Comment
   },
   props: {
     avatar: {
@@ -38,6 +42,10 @@ export default {
     date: {
       type: String,
       required: true
+    },
+    comments: {
+      type: Array,
+      required: false
     }
   },
   data () {
@@ -55,64 +63,65 @@ export default {
 
 <style lang="scss" scoped>
 .feed {
-    margin: 0 0 24px;
-    text-align: left;
+  margin: 0 0 24px;
+  text-align: left;
 
-    &__user {
-        margin-bottom: 6px;
-        .user {
-            display: flex;
-            align-items: center;
-        }
+  &__user {
+    margin-bottom: 6px;
 
-        :deep(.user__name) {
-            font-size: 18px;
-            font-weight: 700;
-            margin-left: 16px;
-        }
-
-        :deep(.user__avatar) {
-            width:44px;
-            height: 44px;
-        }
+    .user {
+      display: flex;
+      align-items: center;
     }
 
-    &__hero {
-        box-shadow: 0px 4px 40px 0px #00000012;
-        border: 1px solid #f1f1f1;
-        padding: 24px 20px 24px 20px;
-        margin-bottom: 18px;
-
-        :deep(.story) p {
-            margin-bottom: 16px;
-        }
-
-        :deep(.story) h2 {
-        font-weight: 700;
-        font-size: 26px;
-        margin-bottom: 16px;
-        }
+    :deep(.user__name) {
+      font-size: 18px;
+      font-weight: 700;
+      margin-left: 16px;
     }
 
-    &__date {
-        text-transform: uppercase;
-        opacity: 0.4;
-        letter-spacing: 0.1em;
-        margin: 10px 0;
-        display: block;
+    :deep(.user__avatar) {
+      width: 44px;
+      height: 44px;
+    }
+  }
+
+  &__hero {
+    box-shadow: 0px 4px 40px 0px #00000012;
+    border: 1px solid #f1f1f1;
+    padding: 24px 20px 24px 20px;
+    margin-bottom: 18px;
+
+    :deep(.story) p {
+      margin-bottom: 16px;
     }
 
-    &__comments {
-        .button {
-            margin: 0 0 10px;
-        }
+    :deep(.story) h2 {
+      font-weight: 700;
+      font-size: 26px;
+      margin-bottom: 16px;
     }
+  }
 
-    @media (max-width:425px) {
-        :deep(.user__avatar) {
-            width: 35px;
-            height: 35px;
-        }
+  &__date {
+    text-transform: uppercase;
+    opacity: 0.4;
+    letter-spacing: 0.1em;
+    margin: 10px 0;
+    display: block;
+  }
+
+  &__comments {
+    .button {
+      margin: 0 0 10px;
     }
+  }
+
+  @media (max-width:425px) {
+    :deep(.user__avatar) {
+      width: 35px;
+      height: 35px;
+    }
+  }
 }
 </style>
